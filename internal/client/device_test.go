d3792afc1716e05322edacd7c76d15dfe156547f
Christopher Barnes <christopher@barnes.biz>
2025-04-15 13:39:08 +0200

    Add tests for CRUD functions

---
+++ internal/client/device_test.go
@@ -0,0 +1,114 @@
+// Copyright (c) Christopher Barnes <christopher@barnes.biz>
+// SPDX-License-Identifier: MPL-2.0
+
+package landb_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	landb "landb/internal/client"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestDeviceCRUD(t *testing.T) {
+	apiEndpoint := "https://landb.cern.ch/api/"
+	clientID := "terraform-provider-landb"
+	clientSecret := "KWGE5p5LbPHY6nQRUNpx2EFJ91fYxYbd"
+	audience := "production-microservice-landb-rest"
+
+	cli, err := landb.NewClient(apiEndpoint, clientID, clientSecret, audience)
+	require.NoError(t, err)
+
+	timestamp := fmt.Sprintf("%d", time.Now().UnixNano())
+	last5 := timestamp[len(timestamp)-5:]
+
+	deviceName := fmt.Sprintf("TF-TEST-DEVICE-%s", last5)
+	serialNumber := fmt.Sprintf("SN%s", last5)
+	inventoryNumber := fmt.Sprintf("INV%s", last5)
+
+	device := landb.Device{
+		Name:                 deviceName,
+		SerialNumber:         serialNumber,
+		InventoryNumber:      inventoryNumber,
+		Tag:                  "TAG001",
+		Description:          "Testing device creation",
+		Zone:                 "ZONE1",
+		DHCPResponse:         "ALWAYS",
+		IPv4InDNSAndFirewall: true,
+		IPv6InDNSAndFirewall: true,
+		ManagerLock:          "NO_LOCK",
+		Ownership:            "CERN",
+		Location: landb.Location{
+			Building: "31",
+			Floor:    "1",
+			Room:     "006",
+		},
+		Parent:       "test",
+		Type:         "COMPUTER",
+		Manufacturer: "APPLE MAC",
+		Model:        "MACBOOK PRO 13",
+		OperatingSystem: landb.OperatingSystem{
+			Family:  "ANDROID",
+			Version: "12",
+		},
+		Manager: landb.Contact{
+			Type: "EGROUP",
+			EGroup: landb.EGroup{
+				Name:  "ai-playground",
+				Email: "christopher.barnes@cern.ch",
+			},
+		},
+		Responsible: landb.Contact{
+			Type: "PERSON",
+			Person: landb.Person{
+				FirstName:  "Christopher",
+				LastName:   "Barnes",
+				Email:      "christopher.barnes@cern.ch",
+				Username:   "chbarnes",
+				Department: "IT",
+				Group:      "CD",
+			},
+		},
+		User: landb.Contact{
+			Type: "PERSON",
+			Person: landb.Person{
+				FirstName:  "Christopher",
+				LastName:   "Barnes",
+				Email:      "christopher.barnes@cern.ch",
+				Username:   "chbarnes",
+				Department: "IT",
+				Group:      "CD",
+			},
+		},
+	}
+
+	t.Logf("Creating device: %s", deviceName)
+	createdDevice, err := cli.CreateDevice(device)
+	require.NoError(t, err)
+	require.Equal(t, device.Name, createdDevice.Name)
+
+	t.Log("Reading device...")
+	readDevice, err := cli.GetDevice(deviceName)
+	require.NoError(t, err)
+	require.Equal(t, createdDevice.Name, readDevice.Name)
+
+	defer func() {
+		t.Logf("Deleting device: %s", createdDevice.Name)
+		err := cli.DeleteDevice(createdDevice.Name, createdDevice.Version)
+		require.NoError(t, err)
+	}()
+
+	t.Log("Updating device...")
+	readDevice.Description = "Updated via test"
+	updatedDevice, err := cli.UpdateDevice(readDevice.Name, *readDevice)
+	require.NoError(t, err)
+	require.Equal(t, "Updated via test", updatedDevice.Description)
+
+	t.Log("Final read to confirm update...")
+	finalDevice, err := cli.GetDevice(deviceName)
+	require.NoError(t, err)
+	require.Equal(t, "Updated via test", finalDevice.Description)
+}
---
+++ internal/client/set_test.go
@@ -0,0 +1,84 @@
+// Copyright (c) Christopher Barnes <christopher@barnes.biz>
+// SPDX-License-Identifier: MPL-2.0
+
+package landb_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	landb "landb/internal/client"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestSetCRUD(t *testing.T) {
+	apiEndpoint := "https://landb.cern.ch/api/"
+	clientID := "terraform-provider-landb"
+	clientSecret := "KWGE5p5LbPHY6nQRUNpx2EFJ91fYxYbd"
+	audience := "production-microservice-landb-rest"
+
+	cli, err := landb.NewClient(apiEndpoint, clientID, clientSecret, audience)
+	require.NoError(t, err)
+
+	timestamp := fmt.Sprintf("%d", time.Now().UnixNano())
+	last5 := timestamp[len(timestamp)-5:]
+
+	setName := fmt.Sprintf("TF-TEST-SET-%s", last5)
+
+	set := landb.Set{
+		Name:                 setName,
+		Type:                 "INTERDOMAIN",
+		NetworkDomain:        "IT-COMPUTING-NETWORK",
+		Description:          "Terraform test set",
+		ProjectURL:           "https://example.com",
+		ReceiveNotifications: true,
+		Responsible: landb.Contact{
+			Type: "PERSON",
+			Person: landb.Person{
+				FirstName:  "Christopher",
+				LastName:   "Barnes",
+				Email:      "christopher.barnes@cern.ch",
+				Username:   "chbarnes",
+				Department: "IT",
+				Group:      "CD",
+			},
+			EGroup: landb.EGroup{
+			Name: "ai-playground",
+			Email: "christopher.barnes@cern.ch",
+			},
+			Reserved: landb.Reserved{
+				FirstName: "Christopher",
+				LastName: "Barnes",
+			},
+		},
+	}
+
+	t.Logf("Creating set: %s", setName)
+	createdSet, err := cli.CreateSet(set)
+	require.NoError(t, err)
+	require.Equal(t, set.Name, createdSet.Name)
+
+	t.Log("Reading set...")
+	readSet, err := cli.GetSet(setName)
+	require.NoError(t, err)
+	require.Equal(t, createdSet.Name, readSet.Name)
+
+	defer func() {
+		t.Logf("Deleting set: %s", createdSet.Name)
+		err := cli.DeleteSet(createdSet.Name, createdSet.Version)
+		require.NoError(t, err)
+	}()
+
+	t.Log("Updating set...")
+	readSet.Description = "Updated set via test"
+	updatedSet, err := cli.UpdateSet(readSet.Name, *readSet)
+	require.NoError(t, err)
+	require.Equal(t, "Updated set via test", updatedSet.Description)
+
+	t.Log("Final read to confirm update...")
+	finalSet, err := cli.GetSet(setName)
+	require.NoError(t, err)
+	require.Equal(t, "Updated set via test", finalSet.Description)
+}
