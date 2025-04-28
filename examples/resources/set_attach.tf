a6def22f4a4b6d66da029ba4b9e51598a4e62f83
Christopher Barnes <christopher@barnes.biz>
2025-04-28 11:24:47 +0200

    Add example for set attach

--- examples/resources/set_attach.tf
+++ examples/resources/set_attach.tf
@@ -1,4 +1,7 @@
-resource "landb_set_attach" "set_attach" {
-  set_id  = landb_set.set.id
-  host_ip = local.ipv4
+resource "landb_set_attach" "example" {
+  set_name    = landb_set.example.name
+  name        = "attachment-01"
+  ipv4        = "192.168.100.100"
+  ipv6        = "2001:db8::100"
+  description = "example interface"
 }
